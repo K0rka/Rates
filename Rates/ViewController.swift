6ae67d3c155ce342790cd3c9e53ad1df16d37267
Catherine Korovkina <c.korovkina@gmail.com>
2019-02-02 11:55:36 +0300

    Initial Commit

---
+++ Rates/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rates
+//
+//  Created by Catherine Korovkina on 2/2/2562 BE.
+//  Copyright © 2562 snm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
